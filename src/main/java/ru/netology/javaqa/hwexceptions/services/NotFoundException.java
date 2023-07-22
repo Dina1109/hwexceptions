27ca9cd9aa919b16dd5926829bdad7040280e3d9
Dina-72 <amenova1109@yandex.ru>
2023-07-22 23:13:19 +0500

    first commit

---
+++ src/main/java/ru/netology/javaqa/hwexceptions/services/NotFoundException.java
@@ -0,0 +1,6 @@
+package ru.netology.javaqa.hwexceptions.services;
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(int id) {
+        super("Товар с id = " + id + " не найден");
+    }
+}
